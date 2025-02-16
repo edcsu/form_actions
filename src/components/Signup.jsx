003af7adf1f816587b0d942b59eae9bae614195b
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-16 12:01:55 +0300

    style: output validation errors

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,12 @@
-import { isEmail, isNotEmpty, hasMinLength, isEqualToOtherValue} from '../util/validation'
+import { useActionState } from 'react'  
+
+import { isEmail, 
+  isNotEmpty, 
+  hasMinLength, 
+  isEqualToOtherValue} from '../util/validation'
+
 export default function Signup() {
-  function signupAction(formData) {
+  function signupAction(prevFormState, formData) {
     const email = formData.get('email')
     const password = formData.get('password')
     const confirmPassword = formData.get('confirm-password')
@@ -39,11 +45,19 @@ export default function Signup() {
     if (acquisitionChannel.length === 0) {
       errors.push('Please select atleast one acquisition channel')
     }
-    console.log(enteredEmail)
+
+    if (errors.length > 0) {
+
+      return { errors: errors }
+    }
+
+    return { errors: null}
   }
 
+  const [ formState, formAction, pending] = useActionState(signupAction, { errors: null})
+
   return (
-    <form action={signupAction}>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -127,6 +141,12 @@ export default function Signup() {
           agree to the terms and conditions
         </label>
       </div>
+
+      {formState.errors && <ul className='error'>
+        {formState.errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>}
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
